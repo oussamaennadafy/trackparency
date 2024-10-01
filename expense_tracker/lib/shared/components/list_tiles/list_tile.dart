34cb64e7405c0421cdaf72e19b8c10d7c49657a8
sam <sam@ora.ma>
2024-10-01 10:09:02 +0100

    make buttons and tiles pressable

--- expense_tracker/lib/shared/components/list_tiles/list_tile.dart
+++ expense_tracker/lib/shared/components/list_tiles/list_tile.dart
@@ -23,87 +23,92 @@ class AppListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Leading icon
-            Container(
-              padding: const EdgeInsets.all(14.0),
-              height: 50,
-              width: 50,
-              decoration: BoxDecoration(
-                color: iconBackgroundColor,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(9999),
-                ),
-              ),
-              child: SvgPicture.asset(icon),
-            ),
-            const SizedBox(width: 16),
-            // Title and subtitle
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: AppColors.primary,
-                    ),
+      child: InkWell(
+        onTap: () {
+          print(title);
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Leading icon
+              Container(
+                padding: const EdgeInsets.all(14.0),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: iconBackgroundColor,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(9999),
                   ),
-                  if (subTitle != null)
-                    Text(
-                      subTitle!,
-                      style: const TextStyle(
-                        color: AppColors.gray,
-                      ),
-                    ),
-                ],
+                ),
+                child: SvgPicture.asset(icon),
               ),
-            ),
-            // Trailing
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
+              const SizedBox(width: 16),
+              // Title and subtitle
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                      "\$",
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: AppColors.gray,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(width: 2),
                     Text(
-                      trailingTitle,
+                      title,
                       style: const TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
                         color: AppColors.primary,
                       ),
                     ),
+                    if (subTitle != null)
+                      Text(
+                        subTitle!,
+                        style: const TextStyle(
+                          color: AppColors.gray,
+                        ),
+                      ),
                   ],
                 ),
-                if (trailingSubTitle != null)
-                  Text(
-                    trailingSubTitle!,
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: AppColors.gray,
-                    ),
+              ),
+              // Trailing
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "\$",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: AppColors.gray,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 2),
+                      Text(
+                        trailingTitle,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ],
                   ),
-              ],
-            ),
-          ],
+                  if (trailingSubTitle != null)
+                    Text(
+                      trailingSubTitle!,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: AppColors.gray,
+                      ),
+                    ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
